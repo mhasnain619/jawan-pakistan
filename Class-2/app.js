a6130b7fea221431185b86d817edfd9ef5616ae6
Muhammad Hasnain <hasnain0893@gmail.com>
2024-11-17 20:40:51 +0500

    class working

--- Class-2/app.js
+++ Class-2/app.js
@@ -51,3 +51,72 @@
 
 // let age = yourAge == 20 ? 'Allow' : 'Not Allow'
 // console.log(age);
+
+
+// let age1 = 20
+// let gender = 'male'
+
+// let check1 = age1 == 20 ? gender == 'male' ? 'allow' : 'gender not match' : 'age not match'
+// console.log(check1);
+
+
+// let age = +prompt('Enter Your age')
+// let username = prompt('Enter Your name')
+
+// let check = age == 20 ? username == 'Anaintay' ? 'yes Its True' : "No Its not True" : "No Its not True"
+// console.log(check);
+
+
+// let percentage = +prompt('Enter your number')
+
+// if (percentage < 101) {
+//     if (
+//         percentage >= 80 && percentage <= 100
+//     ) {
+//         console.log('Grade A++')
+//     } else if (percentage >= 60 && percentage < 80) {
+//         console.log('Grade B')
+//     } else {
+//         console.log('Grade C')
+//     }
+// } else {
+//     console.log('not Allowed')
+// }
+// let percentage = +prompt('Enter your number')
+
+// let check = percentage >= 33 && percentage <= 50 ? "E grade": 
+// percentage >= 51 && percentage <= 66 ? "D grade":
+// percentage >= 67 && percentage <= 80 ? "C grade":
+// percentage >= 81 && percentage <= 100 ? "B grade" : "A grade"
+// console.log(check)
+
+
+
+// let percentage = +prompt('Enter your Percentage');
+
+// if a(percentage >= 33 && percentage <= 50) {
+//     console.log('Grde D');
+// } else if (percentage >= 51 && percentage <= 60) {
+//     console.log('Grade C');
+// }
+
+
+// let percentage = +prompt('Enter Your Percantege')
+
+// let check = percentage >= 33 && percentage <= 50 ? "Grade D" :
+//     percentage >= 51 && percentage <= 60 ? "Grade C" :
+//         percentage >= 61 && percentage <= 70 ? "Grade B" :
+//             percentage >= 71 && percentage <= 80 ? "Grade A" :
+//                 percentage >= 81 && percentage <= 100 ? "Grade A++" : "Invalid Percentage"
+// console.log(check)
+
+
+let percentage = +prompt('Enter Your Percantege')
+
+let check = percentage >= 31 && percentage <= 50 ? "grade D" :
+    percentage >= 51 && percentage <= 60 ? "grade c" :
+        percentage >= 61 && percentage <= 70 ? "grade b" :
+            percentage >= 71 && percentage <= 80 ? "grade a" :
+                percentage >= 81 && percentage <= 100 ? "grade a++" : "invalid percantage"
+console.log(check);
+
